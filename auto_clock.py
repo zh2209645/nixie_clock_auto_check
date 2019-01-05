a8a614c18bd9c02c02a257021386fadee96290f2
zh2209645 <qianpau@qianp.cc>
2019-01-05 21:41:38 +0800

    Add files via upload

---
+++ auto_clock.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import sys
+import clock_supp
+import time
+
+time_sleep = '3600'
+auto_close_connection = False
+cl = clock_supp.Nixie_Clock()
+
+def check_time(Nixie_Clock_Object):
+    if not Nixie_Clock_Object.is_open():
+        Nixie_Clock_Object.open()
+    t = time.time()
+    print('---------------------')
+    print(time.asctime(time.localtime(t)) + ' check clock time...')
+    Nixie_Clock_Object.save_time(t)
+    if auto_close_connection:
+        Nixie_Clock_Object.close()
+    print('Finish')
+    print('---------------------')
+
+
+if __name__ == "__main__":
+    if cl.is_open():
+        cl.close()
+    
+    if len(sys.argv) != 1:
+        time_sleep = int(sys.argv[1])
+    
+    while True:
+        check_time(cl)
+        time.sleep(time_sleep)
---
+++ clock_supp.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python3
+
+import serial
+import time
+
+class Nixie_Clock:
+
+    baud_rate = 115200
+    port = '/dev/ttyACM0'
+    light_color = ['FF8C00', 'FF8C00', 'FF8C00', 'FF8C00', 'FF8C00', 'FF8C00']
+    
+    ser = None
+    timeout = 0.5
+
+    def __init__(self):
+        self.read_config()
+        self.ser = serial.Serial(port=self.port, baudrate=self.baud_rate, timeout=self.timeout)
+    
+    def change_baud_rate(self, baud_rate=int()):
+        self.baud_rate = baud_rate
+        self.save_config()
+
+    def change_port(self, port=str()):
+        self.port = port
+        self.save_config()
+
+    def change_color(self, light_index, color_str):
+        self.light_color[light_index] = color_str
+        self.save_config()
+    
+    def save_time(self, timestamp=time.time()):
+        time_data = self.__time_convert(timestamp)
+        write_data = '*'
+
+        for i in range(6):
+            write_data = write_data + time_data[i] + self.light_color[i]
+        
+        self.write_to_clock(write_data.encode())
+
+    def __time_convert(self, timestamp):
+        struct_time = time.localtime(timestamp)
+        time_list = ['0', '0', '0', '0', '0', '0']
+        
+        tm_hour = str(struct_time.tm_hour)
+        if len(tm_hour) == 1:
+            time_list[0] = '0'
+        else:
+            time_list[0] = tm_hour[0]
+        time_list[1] = tm_hour[-1]
+        
+        tm_min = str(struct_time.tm_min)
+        if len(tm_min) == 1:
+            time_list[2] = '0'
+        else:
+            time_list[2] = tm_min[0]
+        time_list[3] = tm_min[-1]
+
+        tm_sec = str(struct_time.tm_sec)
+        if len(tm_sec) == 1:
+            time_list[4] = '0'
+        else:
+            time_list[4] = tm_sec[0]
+        time_list[5] = tm_sec[-1]
+        
+        return time_list
+
+    def connection(self):
+        if self.ser.isOpen():
+            print("Connected")
+        else:
+            print('Disconnected')
+
+    def close(self):
+        if self.ser.isOpen():
+            self.ser.close()
+            print("Connection closed")
+        else:
+            print("Connection already closed")
+        
+    def open(self):
+        if self.ser.isOpen():
+            print("Already connected")
+        else:
+            self.ser.open()
+            print("Connection opened")
+    
+    def is_open(self):
+        return self.ser.isOpen()
+
+    def write_to_clock(self, bytes_data):
+        self.ser.write(bytes_data)
+        for line in self.ser.readlines():
+            print(line.decode())
+
+    def recv_data(self):
+        for line in self.ser.readlines():
+            print(line.decode())
+
+    def save_config(self):
+        with open('n_clock.config', 'w') as file_to_write:
+            file_to_write.write(str(self.baud_rate))
+            file_to_write.write('\n')
+            file_to_write.write(self.port)
+            file_to_write.write('\n')
+            l_color = ' '.join(self.light_color)
+            file_to_write.write(l_color)
+
+    def read_config(self):
+        try:
+            setting = list()
+            with open('n_clock.config', 'r') as file_to_read:
+                for line in file_to_read.readlines():
+                    line = line.strip()
+                    setting.append(line)
+            self.load_setting_from_list(setting)
+            
+        except FileNotFoundError as err:
+            print(err)
+            print('Nixie clock Profile does not exist')
+            print('Using default setting...')
+            self.save_config()
+
+    def load_setting_from_list(self, setting=list()):
+        if len(setting) != 0:
+            print('-------- load setting --------')
+            self.baud_rate = setting[0]
+            print('Set baud rate {}'.format(self.baud_rate))
+            self.port = setting[1]
+            print('Set port {}'.format(self.port))
+            l_color = setting[2].split(' ')
+            self.light_color = l_color
+            print('Set light color {}'.format(self.light_color))
